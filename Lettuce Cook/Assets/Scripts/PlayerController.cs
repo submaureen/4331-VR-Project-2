f85690d5daaf669ef370cbd5547a1f721b4832fc
Maureen Gloria <maureenmgloria@gmail.com>
2020-11-12 17:12:15 -0600

    add camera panning

---
+++ Lettuce Cook/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Transform playerCamera = null;
+
+    float cameraPitch = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouseLook();
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        cameraPitch -= mouseDelta.y * 2;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * mouseDelta.x * 2);
+
+    }
+}
