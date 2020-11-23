a0d3961fa5cb20b3217c7e470c6e580d422de8f9
DJ <dj9123@live.com>
2020-11-23 17:43:37 -0600

    Add base RecipeManagement script

---
+++ Lettuce Cook/Assets/Scripts/RecipeManagement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeManagement : MonoBehaviour
+{
+
+    float currCountdownValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+        Debug.Log("Countdown: " + currCountdownValue);
+        DoLast();
+    }
+
+    public void DoLast()
+    {
+        Debug.Log("test");
+        //yield return null;
+    }
+}
