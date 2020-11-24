ee29b5c3761862a212b144ac5a2e18f80e77da87
DJ <dj9123@live.com>
2020-11-23 22:38:53 -0600

    maur+ dj: Add cooking with programmer defined recipe example

---
+++ Lettuce Cook/Assets/ObjectTypes/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.ObjectTypes
+{
+    [Serializable]
+    class Recipe
+    {
+        public string name;
+        public Step[] steps;
+    }
+}
