5fc3abef057335f8b3464255e863c397fa97cb3c
Maureen Gloria <maureenmgloria@gmail.com>
2020-11-28 16:18:47 -0600

    Add timer for pouring

--- Lettuce Cook/Assets/Scripts/ContainerCollisions.cs
+++ Lettuce Cook/Assets/Scripts/ContainerCollisions.cs
@@ -24,7 +24,6 @@ public class ContainerCollisions : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
-        print(collider);
         if (collider.gameObject.tag == "ingredient" && UtensilCollision.prepDone)
         {
             if (step.ingredient == collider.gameObject.name)
---
+++ Lettuce Cook/Assets/Scripts/ParticleHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleHandler : MonoBehaviour
+{
+
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        time = 0;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        time += Time.deltaTime;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($" poured for {time * 100}");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collide");
+    }
+}
