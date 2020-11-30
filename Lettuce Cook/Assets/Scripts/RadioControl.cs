3e53423b26794000371f22ff08ee976f3c4f76bf
DJ <dj9123@live.com>
2020-11-29 19:56:05 -0600

    Add radio and window sounds

---
+++ Lettuce Cook/Assets/Scripts/RadioControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioControl : MonoBehaviour
+{
+
+    [SerializeField]
+    public AudioSource button_push;
+
+    public delegate void ClickAction();
+    public static event ClickAction playAudio;
+    public static event ClickAction stopAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopAudio()
+    {
+        stopAudio();
+        button_push.Play();
+    }
+
+    public void PlayAudio()
+    {
+        playAudio();
+        button_push.Play();
+    }
+}
